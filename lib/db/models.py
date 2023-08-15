5aafba8cd05b16e3195ea644c0aca3dd8d2a540e
Kevin Bravo <kbravo1024@gmail.com>
2023-08-14 19:53:53 -0700

    add class doctor

--- lib/db/models.py
+++ lib/db/models.py
@@ -2,3 +2,8 @@ from sqlalchemy import ForeignKey, Table, Integer, Column, String
 from sqlalchemy.orm import relationship, backref, declarative_base
 
 Base = declarative_base()
+#create a class doctor
+class Doctor(Base):
+    __tablename__ = 'doctors'
+
+    id = Column(Integer(), primary_key=True)
