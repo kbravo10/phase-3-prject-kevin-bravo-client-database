9bda94acb78b431d5a17205cbca126849e82826d
Kevin Bravo <kbravo1024@gmail.com>
2023-08-16 15:48:07 -0700

    add ligin and exit methods

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -15,21 +15,25 @@ from  models import Doctor, Med_times, Client, Medication
 Base = declarative_base()
 
 if __name__ == "__main__":
-    
+    #variable that keeps track of person logged in
+    current_user = None
+
     #method start to prompt the user how to start and the options
     def start():
         print('\n' * 10)
 
         print('Welcome... Please choose an option: \n')
-        options = ['Clients', 'Medications', 'Doctors', 'Medication Times']
+        options = ['Login', 'Exit']
         terminal_menu = TerminalMenu(options)
         menu_entry_index = terminal_menu.show()
         print(green(f'You have selected {options[menu_entry_index]}'))
 
         print('\n' *5)
 
-    def handle_user_input():
+    def login():
         pass
+    def exit():
+        print('Thank you. Bye!')
     
 
     start()
