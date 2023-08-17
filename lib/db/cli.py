63e98f9a8629fd8e7d917fabf46943a08b96a485
Kevin Bravo <kbravo1024@gmail.com>
2023-08-17 13:06:57 -0700

    create method to handle medication schedule, install pandas

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -8,6 +8,7 @@ from faker import Faker
 #import random to random fill values
 import random
 import time
+import pandas as pd
 import ipdb
 from prettycli import red, green
 from simple_term_menu import TerminalMenu
@@ -23,6 +24,10 @@ class Cli():
         Session = sessionmaker(bind=engine)
         session = Session()
         return session
+    
+    #method to help with the cluter by printing empty lines
+    def clear_screen(self):
+        print('\n' * 3)
 
     #method start to prompt the user how to start and the options
     def start(self):
@@ -42,9 +47,7 @@ class Cli():
         
         self.clear_screen()
     
-    #method to help with the cluter by printing empty lines
-    def clear_screen(self):
-        print('\n' * 3)
+   
 
     #lets the user login if login was chosen
     def login(self):
@@ -77,6 +80,16 @@ class Cli():
             print('You have chosen doctors.')
             time.sleep(1)
             self.handle_doctor_choice()
+        #the user selects medication, take them to handle_medication_choice
+        elif options[menu_entry_index] == 'Medications':
+            print('You have chosen Medication.')
+            time.sleep(1)
+            self.handle_medication_choice()
+        #user selects Medication schedule
+        else:
+            print('You have selected medication schedule.')
+            time.sleep(1)
+            self.handle_med_schedule_choice()
 
         self.clear_screen()
     
@@ -130,6 +143,71 @@ class Cli():
         session = self.session_creator()
         doctors = session.query(Doctor)
         self.clear_screen()
+
+        #handle option to see all the doctors
+        if options[menu_entry_index] == 'View all doctors':
+            print(doctors.all())
+        #handle search by name
+        elif options[menu_entry_index] == 'Search by name':
+            user_input = input('Enter the doctors name: ')
+            doctor_filter_name = doctors.filter(Doctor.name == user_input)
+
+            if doctor_filter_name.count() != 0:
+                for doc in doctor_filter_name:
+                    print(doc)
+            else:
+                print(red('There is no doctor with that name on the database.'))
+        elif options[menu_entry_index] == 'Search by ID':
+            user_input = input('Enter the doctors ID: ')
+            doctor_filter_id = doctors.filter(Doctor.id == user_input)
+            
+            if doctor_filter_id.count() != 0:
+                print(doctor_filter_id[0])
+            else:
+                print(red('There is no doctor with this ID.'))
+    def handle_medication_choice(self):
+        self.clear_screen()
+
+        #display options for medications class and prompt user to select
+        options = ['View all medications', 'Search by name', 'Search by ID']
+        terminal_menu = TerminalMenu(options)
+        menu_entry_index = terminal_menu.show()
+        
+        #create a session with the Doctor class
+        session = self.session_creator()
+        medications = session.query(Medication)
+        self.clear_screen()
+
+        #print all of the medications if user wants to view all medications
+        if options[menu_entry_index] == 'View all medications':
+            print('list of all medications')
+            print(pd.DataFrame(medications.all()))
+        #Allow the user to enter a specific name and look for that medications information
+        elif options[menu_entry_index] == 'Search by name':
+            user_input = input('What is the medications name: ')
+            medication_filter_name = medications.filter(Medication.name == user_input)
+            self.clear_screen()
+            if medication_filter_name.count() != 0:
+                print('Medications(s):')
+                for meds in medication_filter_name:
+                    print(meds)
+            else:
+                print(red('There is no medication with that name.'))
+        #Allow the user to search a medications with there id number
+        elif options[menu_entry_index] == 'Search by ID':
+            user_input = input('What is the medication id: ')
+            medication_filter_id = medications.filter(Medication.id == user_input)
+            if medication_filter_id.count() != 0:
+                print(medication_filter_id[0])
+            else:
+                print(red('There is no medication with that id.'))
+    
+    def handle_med_schedule_choice(self):
+        self.clear_screen()
+
+        #display options for user to choose medication schedule
+
+        
 
 
 
